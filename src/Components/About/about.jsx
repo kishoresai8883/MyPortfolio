929a43e833b6b3154a9572c99600f7ba5e2d2a2b
kishoresai <kishore.sai207@gmail.com>
2024-03-17 13:45:15 +0530

    About page implemented

--- src/Components/About/about.jsx
+++ src/Components/About/about.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
+import './about.css'
+import Me from '../../Assets/me-about.jpg'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 const About = () => {
   return (
     <section id='about'>
       <h5>Get To Know</h5>
       <h2>About Me</h2>
+      <div className="container about-container">
+        <div className="about-me">
+          <img src={Me} alt="About img" className="about-me-img" />
+        </div>
+        <div className="about-content">
+          <div className="about-cards">
+            <article className='about-card'>
+              <FaAward className='about-icon'/>
+              <h5>Experience</h5>
+              <small>1+ Year's Working</small>
+            </article>
+            <article className='about-card'>
+              <FiUsers className='about-icon'/>
+              <h5>Clients</h5>
+              <small>20+ Worldwide</small>
+            </article>
+            <article className='about-card'>
+              <VscFolderLibrary className='about-icon'/>
+              <h5>Projects</h5>
+              <small>30+ Completed</small>
+            </article>
+          </div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Error, a molestiae? Obcaecati reiciendis excepturi ipsam quas, 
+            nobis quo cumque adipisci similique iste architecto eaque officiis, 
+            deleniti sint fugiat corporis error?
+          </p>
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
     </section>
   )
 }
--- src/Components/Experience/experience.jsx
+++ src/Components/Experience/experience.jsx
@@ -1,9 +1,101 @@
 import React from 'react'
+import { BsPatchCheckFill } from "react-icons/bs";
+import './experience.css'
 
 const Experience = () => {
   return (
     <section id='experience'>
-      Experience
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+      <div className="container exp-container">
+        <div className="exp-frontend">
+          <h3>Frontend Development</h3>
+          <div className="exp-content">
+            <article className="exp-details">
+              <BsPatchCheckFill className='exp-details-icon'/>
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className="exp-details">
+              <BsPatchCheckFill className='exp-details-icon'/>
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className="exp-details">
+              <BsPatchCheckFill className='exp-details-icon'/>
+              <div>
+                <h4>Javascript</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className="exp-details">
+              <BsPatchCheckFill className='exp-details-icon'/>
+              <div>
+                <h4>Bootstrap</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className="exp-details">
+              <BsPatchCheckFill className='exp-details-icon'/>
+              <div>
+                <h4>Tailwind</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className="exp-details">
+              <BsPatchCheckFill className='exp-details-icon'/>
+              <div>
+                <h4>React</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="exp-backend">
+          <h3>Backend Development</h3>
+          <div className="exp-content">
+          <article className="exp-details">
+              <BsPatchCheckFill className='exp-details-icon'/>
+              <div>
+                <h4>Nodejs</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className="exp-details">
+              <BsPatchCheckFill className='exp-details-icon'/>
+              <div>
+                <h4>Mongodb</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className="exp-details">
+              <BsPatchCheckFill className='exp-details-icon'/>
+              <div>
+                <h4>Javascript</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className="exp-details">
+              <BsPatchCheckFill className='exp-details-icon'/>
+              <div>
+                <h4>Python</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className="exp-details">
+              <BsPatchCheckFill className='exp-details-icon'/>
+              <div>
+                <h4>MySql</h4>
+                <small className='text-light'>Basic</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
