e0aa58f9681f04e3d7e56daf0b3741d0bd02c5ae
kishoresai <kishore.sai207@gmail.com>
2024-06-17 23:01:32 +0530

    navigation terms changes

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -7,13 +7,13 @@ import { FaGithub } from 'react-icons/fa'
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className="footer-logo">Kishore</a>
+      <a href="#home" className="footer-logo">Kishore</a>
       <ul className="perma-links">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Experience</a></li>
-        <li><a href="#">Portfolio</a></li>
-        <li><a href="#">Contact</a></li>
+        <li><a href="home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
       <div className='footer-socials'>
         <a href="https://instagram.com/kishoresai_g" target='_blank' rel="noreferrer"><FiInstagram/></a>
