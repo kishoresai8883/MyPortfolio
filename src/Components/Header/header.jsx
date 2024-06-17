91c5d6894cfcfacd76a4fb66b48e776e5b4fdd01
kishoresai <kishore.sai207@gmail.com>
2024-06-17 21:33:46 +0530

    name change

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     <header>
       <div className="container header_container">
         <h5>Hello, I'm</h5>
-        <h1>Kishore</h1>
+        <h1>Kishore Sai</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA/>
         <HeaderSocials/>
