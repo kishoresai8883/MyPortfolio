788297049b0f792030e1618582b1ae19f1a5eae1
Kishore Sai <kishore.sai207@gmail.com>
2025-07-07 23:34:23 +0530

    portfolio updated

--- src/Components/Portfolio/portfolio.jsx
+++ src/Components/Portfolio/portfolio.jsx
@@ -5,49 +5,49 @@ import Img2 from '../../Assets/portfolio2.jpg';
 import Img3 from '../../Assets/portfolio3.jpg';
 import Img4 from '../../Assets/portfolio4.jpg';
 import Img5 from '../../Assets/portfolio5.png';
-import Img6 from '../../Assets/chat.png';
+import Img6 from '../../Assets/portfolio6.png';
 
 const data = [
     {
         id: 1,
         image: Img1,
         title: 'NxtTrendz E-Commerce Application',
-        github: 'https://github.com',
+        github: 'https://github.com/kishoresai8883',
         demo: 'https://gknxttrendz.ccbp.tech'
     },
     {
         id: 2,
         image: Img6,
-        title: 'Real-Time Chat Application',
-        github: 'https://github.com',
-        demo: 'https://dribbble.com/shots/21332880-Chat-Mobile-App'
+        title: 'Bluewave GIS & Manpower Solutions',
+        github: 'https://github.com/kishoresai8883',
+        demo: 'https://bluewavegis.com'
     },
     {
         id: 3,
         image: Img2,
         title: 'Simple Stop-watch Application',
-        github: 'https://github.com',
+        github: 'https://github.com/kishoresai8883',
         demo: 'https://gkstopwatch.ccbp.tech'
     },
     {
         id: 4,
         image: Img3,
         title: 'Wiki Search Application',
-        github: 'https://github.com',
+        github: 'https://github.com/kishoresai8883',
         demo: 'https://kishoresaiwiki.ccbp.tech'
     },
     {
         id: 5,
         image: Img4,
         title: 'Todo Application',
-        github: 'https://github.com',
+        github: 'https://github.com/kishoresai8883',
         demo: 'https://kishoresaitodo.ccbp.tech'
     },
     {
         id: 6,
         image: Img5,
         title: 'Simple Cash Withdrawal Demo',
-        github: 'https://github.com',
+        github: 'https://github.com/kishoresai8883',
         demo: 'https://gkcashwithdraw.ccbp.tech'
     }
 ]
@@ -58,7 +58,7 @@ const Portfolio = () => {
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
       <div className="container portfolio-container">
-        { data.map(({id,image,title,github,demo}) => {
+        { data.map(({id, image, title, github, demo}) => {
             return(
                 <article className="portfolio-item">
                     <div className="portfolio-item-img">
